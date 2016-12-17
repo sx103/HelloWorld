b133a80eb09dc863d20ed6ab7f5ebbf6f9cf1173
sx103 <kenneth.shu@139.com>
2016-12-17 20:30:22 +0800

    Test check in to GitHub. #1

--- src/HelloWorldApp.java
+++ src/HelloWorldApp.java
@@ -5,6 +5,7 @@
 public class HelloWorldApp {
     public static void main(String args[])
     {
+        // test comment to check in GitHub
         HelloWorldParm.getInstance().setName("Wing");
 
         System.out.println("This is my first class developed in IntelliJ IDEA!\n" + HelloWorldParm.getInstance().getName());
